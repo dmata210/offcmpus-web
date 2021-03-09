6719942e80f91ec06df446d19bd068ff284e5990
Abdul-Muiz Yusuff <58189969+sacrael@users.noreply.github.com>
2021-03-09 03:04:21 -0500

    Added prompt to confirm email for student before access is restricted

--- src/views/SearchView.tsx
+++ src/views/SearchView.tsx
@@ -383,6 +383,8 @@ const SearchView = () => {
             {/* Right Side */}
             {properties.length > 0 &&
             <div className="right-side_">
+                <StudentConfirmPrompt />
+
                 {/* <NewSearchResult result={properties[0]} /> */}
                 {properties.map((property: PropertySearchResult, i: number) => 
                     <NewSearchResult 
@@ -678,6 +680,21 @@ const NewSearchResult = ({result, onNavigate, onNavigateInstitute}:
             </div>
         </div>
 
+    </div>)
+}
+
+const StudentConfirmPrompt = () => {
+
+    return (<div className="student-confirm-prompt">
+        <div className="header_">Confirm Your Email</div>
+        <div className="body_">
+            You must confirm your email within 24-hours to continue
+            using this service. You will have restricted access until
+            your <span className="code">.edu</span> email has been confirmed.
+        </div>
+        <div className="btn-area">
+            <div className="_btn1_">Resend Confirmation Email</div>
+        </div>
     </div>)
 }
 
